4cc3ce920e12c00a14632775cf50eb2ccc78efbd
Utkarsh Singh <utkarshsingh2015@gmail.com>
2022-04-04 15:19:04 +0530

    server fixed

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, './client/build')));
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/build', 'index.html'));
-});
 
 app.use('/api/auth', require("./routes/auth.routes.js"))
 app.use('/api/train', require("./routes/train.routes.js"))
 // app.use(verifyAuthentication)
 app.use('/api/booking', require("./routes/booking.routes.js"))
 
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, './client/build', 'index.html'));
+});
 
 module.exports = app
